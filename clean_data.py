fe44ee037fe5e818c882d055177ce17e4ce31b8b
vozellad <vozellad@protonmail.com>
2024-11-07 21:25:28 -0500

    Code is now for updated dataset and goal

--- clean_data.py
+++ clean_data.py
@@ -5,60 +5,72 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 
 def clean_data(df):
-    df.drop(["prodt", "score_plus_1"], axis=1, inplace=True)
+    # Clean the column names (specifically 'Daytime/evening attendance')
+    df.columns = df.columns.str.strip().str.replace('\t', '')
+    df.columns = df.columns.str.replace('"', '')
 
-    df.drop(df[df['pstl_yr'] < 2023].index, inplace=True)
-
-    columns_to_check = ['orgn_area', 'orgn_dist', 'destn_area', 'destn_dist']
-    # drop rows with -1 value
-    condition = df[columns_to_check].isin([-1]).any(axis=1)
-    df.drop(df[condition].index, inplace=True)
-
+    # Remove duplicates.
     df.drop_duplicates(inplace=True)
 
-    df['avg_days_to_delr'] = df['avg_days_to_delr'].round(10)
-    df['score'] = df['score'].round(10)
+    # Change classified text under ‘Target’ column to be integers.
+    df['Target'] = LabelEncoder().fit_transform(df['Target'])
 
-    df.dropna(subset=['avg_days_to_delr', 'score'], inplace=True)
+    # Strip leading and trailing white-spaces.
+    df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
 
+    # Drop empty rows.
     df.dropna(how='all', inplace=True)
 
+    # Replace missing values with the mean.
+    df.fillna(df.mean(), inplace=True)
 
-def visualize_data(df, column_title):
-    # shape
-    plt.figure(figsize=(12, 5))
+    # Set proper precision.
+    df = df.round(3)
 
-    # histogram
-    plt.subplot(1, 2, 1)
-    sns.histplot(df[column_title], bins=100, kde=True)
-    plt.ylabel('Frequency')
+    return df
 
-    # boxplot
-    plt.subplot(1, 2, 2)
-    sns.boxplot(y=df[column_title])
 
-    plt.tight_layout()
+def visualize_data(df):
+    df['Target'].value_counts().plot(kind='pie', autopct='%1.3f%%')
+    plt.title('Pie chart of Target')
     plt.show()
+
+
+def iqr_processing(df):
+    variance = df.var()
+    std_dev = df.std()
+    q1 = df.quantile(0.25)
+    q3 = df.quantile(0.75)
+    iqr = q3 - q1
+
+    print('Variance:')
+    print(variance, '\n')
+    print('Standard Deviation:')
+    print(std_dev, '\n')
+    print('Inter-quartile Range (IQR):')
+    print(iqr, '\n')
+
+    print('\n\n')
+
+    outliers = df[(df < (q1 - 1.5 * iqr)) | (df > (q3 + 1.5 * iqr))]
+    print("Outliers:")
+    print(outliers)
+    outliers.to_csv("outliers.xlsx", index=False)
+
 
 
 def main():
     filename = "Predict Student Dropout and Academic Success.csv"
-    df = pd.read_csv(filename)
-
-    output_filename = "cleaned_data.xlsx"
-    df.to_csv(output_filename, index=False)
-
+    df = pd.read_csv(filename, delimiter=';')
     clean_data(df)
+    df.to_csv("cleaned_data.xlsx", index=False)
 
-    x = df['']
-    y = df['avg_days_to_delr']
-
-    visualize_data(df, 'score')
-    visualize_data(df, 'avg_days_to_delr')
-
+    iqr_processing(df)
+    visualize_data(df)
     print(df.describe())
 
 
