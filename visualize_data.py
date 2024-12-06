f8d57635950d0aeedb9c220e016be3b160529156
vozellad <vozellad@protonmail.com>
2024-12-06 11:56:04 -0500

    Save plots to file and make function that does end plot operations to reduce code duplication

--- visualize_data.py
+++ visualize_data.py
@@ -1,22 +1,22 @@
+import os
+import re
+
 from matplotlib import pyplot as plt
-from sklearn.metrics import pairwise_distances
 import seaborn as sns
 import pandas as pd
 
 
 def plot_target_pie_chart(df):
     df['Target'].value_counts().plot(kind='pie', autopct='%1.3f%%')
-    plt.title('Pie chart of Target')
-    plt.show()
+    _plot_ending('Pie chart of Target')
 
 
 def plot_distance_curricular_units(data, title):
     feature = 'Curricular units 2nd sem (approved)'
     plt.figure(figsize=(10, 6))
     sns.histplot(data.X[feature], bins=20, kde=True, color='blue')
-    plt.title(f'Distribution of {feature} - {title}')
     plt.ylabel('Frequency')
-    plt.show()
+    _plot_ending(f'Distribution of {feature} - {title}')
 
 
 def plot_confusion_matrix(matrix, title):
@@ -24,8 +24,7 @@ def plot_confusion_matrix(matrix, title):
     sns.heatmap(matrix, annot=True, fmt="d", cmap="Blues", cbar=False)
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
-    plt.title(f'Confusion Matrix - {title}')
-    plt.show()
+    _plot_ending(f'Confusion Matrix - {title}')
 
 
 def plot_classification_report(report, title):
@@ -35,8 +34,7 @@ def plot_classification_report(report, title):
 
     plt.figure(figsize=(8, 6))
     sns.heatmap(report, annot=True, cmap="YlGnBu", cbar=False, fmt=".2f")
-    plt.title(f'Classification Report Heatmap - {title}')
-    plt.show()
+    _plot_ending(f'Classification Report Heatmap - {title}')
 
 
 def plot_residuals(y_test, y_pred, title):
@@ -46,8 +44,7 @@ def plot_residuals(y_test, y_pred, title):
     plt.axhline(y=0, color='red', linestyle='--')
     plt.xlabel('Predicted Values')
     plt.ylabel('Residuals')
-    plt.title(f'Residual Plot - {title}')
-    plt.show()
+    _plot_ending(f'Residual Plot - {title}')
 
 
 def plot_feature_importance(model, feature_names, title):
@@ -58,9 +55,17 @@ def plot_feature_importance(model, feature_names, title):
     feature_importances = feature_importances.sort_values(by='Importance')
 
     plt.figure(figsize=(12, 10))
-    plt.title(f'Feature Importances - {title}')
     sns.barplot(x='Importance', y='Feature', data=feature_importances, palette='Blues_d', hue='Feature')
     plt.xlabel('Importance')
     plt.ylabel('Feature')
     plt.subplots_adjust(left=0.3)
+    _plot_ending(f'Feature Importances - {title}')
+
+
+def _plot_ending(title):
+    os.makedirs('plots', exist_ok=True)
+    title = re.sub(r'[<>:"/\\|?*\s]', '_', title)
+    plt.title(title)
+    plt.savefig(f'plots/{title}.png')
     plt.show()
+    plt.close()
