cc3022850a63d66c1ca1a6a9a5c6d3515cb8dbb2
vozellad <vozellad@protonmail.com>
2024-12-05 19:52:19 -0500

    create function intended to display processed data in webpage

--- web_app.py
+++ web_app.py
@@ -2,6 +2,13 @@ from zipfile import BadZipFile
 from flask import Flask, request, render_template
 import pandas as pd
 import openpyxl  # engine used in 'pd.read_excel'
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+
+from ai_utils import iqr_processing, prepare_data, eval_classification
+from predict_student_success import clean_data
+from visualize_data import plot_target_pie_chart
 
 app = Flask(__name__)
 
@@ -21,8 +28,20 @@ def upload_file():
             except BadZipFile:
                 df = pd.read_csv('temp.xlsx')
             table_html = df.to_html(classes='table table-striped')
+            process_and_visualize_data(df)
 
     return render_template('index.html', prediction=table_html)
+
+
+def process_and_visualize_data(df):
+    df = clean_data(df)
+    iqr_processing(df)
+    plot_target_pie_chart(df)
+    df.describe()
+    data_splits = prepare_data(df)
+    eval_classification(LogisticRegression(), data_splits, 'Logistic Regression')
+    eval_classification(RandomForestClassifier(), data_splits, 'Random Forest')
+    eval_classification(KNeighborsClassifier(), data_splits, 'KNN')
 
 
 if __name__ == '__main__':
